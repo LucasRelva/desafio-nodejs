cd4d94d032314654351d3745676804a3e1a5ee73
LucasRelva <lucas.relva@evolutix.com.br>
2024-06-09 23:51:52 -0300

    Finishing tasks endpoints

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,36 +1,91 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, HttpStatus, UseGuards, Request } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { PaginatedTaskDto } from './dto/paginated-task.dto';
+import { ApiTags, ApiBody, ApiResponse, ApiParam, ApiQuery, ApiBearerAuth, ApiBadRequestResponse, ApiNotFoundResponse, ApiInternalServerErrorResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { AddTagsDto } from './dto/add-tags.dto';
+import { AddAssigneeDto } from './dto/add-assignee.dto';
 
-@ApiTags('task')
-@Controller('task')
+@ApiTags('tasks')
+@Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
   @Post()
-  create(@Body() createTaskDto: CreateTaskDto) {
-    return this.taskService.create(createTaskDto);
+  @ApiBody({ type: CreateTaskDto })
+  @ApiBearerAuth()
+  @ApiResponse({ type: CreateTaskDto, status: HttpStatus.CREATED, description: 'The task has been successfully created.' })
+  @ApiBadRequestResponse({ description: 'Invalid data.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async create(@Body() createTaskDto: CreateTaskDto, @Request() req) {
+    return await this.taskService.create(createTaskDto, req.headers.authorization);
   }
 
   @Get()
-  findAll() {
-    return this.taskService.findAll();
+  @ApiBearerAuth()
+  @ApiQuery({ name: 'page', required: true, description: 'Page number', type: Number })
+  @ApiQuery({ name: 'size', required: true, description: 'Page size', type: Number })
+  @ApiResponse({ type: PaginatedTaskDto, status: HttpStatus.OK, description: 'Successfully fetched tasks.' })
+  @ApiBadRequestResponse({ description: 'Invalid query parameters.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async findAll(@Query('page') page: number, @Query('size') size: number) {
+    return await this.taskService.findAll(page, size);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.taskService.findOne(+id);
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', required: true, description: 'Task ID' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successfully fetched task.' })
+  @ApiNotFoundResponse({ description: 'Task not found.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async findOne(@Param('id') id: number) {
+    return await this.taskService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTaskDto: UpdateTaskDto) {
-    return this.taskService.update(+id, updateTaskDto);
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', required: true, description: 'Task ID' })
+  @ApiBody({ type: UpdateTaskDto })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Successfully updated task.' })
+  @ApiNotFoundResponse({ description: 'Task not found.' })
+  @ApiBadRequestResponse({ description: 'Invalid data.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async update(@Param('id') id: number, @Body() updateTaskDto: UpdateTaskDto) {
+    return await this.taskService.update(+id, updateTaskDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.taskService.remove(+id);
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', required: true, description: 'Task ID' })
+  @ApiResponse({ status: HttpStatus.NO_CONTENT, description: 'Successfully deleted task.' })
+  @ApiNotFoundResponse({ description: 'Task not found.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async remove(@Param('id') id: number) {
+    return await this.taskService.remove(+id);
+  }
+
+  @Post(':id/tags')
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', required: true, description: 'Task ID' })
+  @ApiBody({ type: AddTagsDto })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Tags successfully added to the task.' })
+  @ApiNotFoundResponse({ description: 'Task not found.' })
+  @ApiBadRequestResponse({ description: 'Invalid data or unauthorized action.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async addTags(@Param('id') id: number, @Body() addTagsDto: AddTagsDto) {
+    return await this.taskService.addTagsToTask(+id, addTagsDto);
+  }
+
+  @Post(':id/assignees')
+  @ApiBearerAuth()
+  @ApiParam({ name: 'id', required: true, description: 'Task ID' })
+  @ApiBody({ type: AddAssigneeDto })
+  @ApiResponse({ status: HttpStatus.OK, description: 'Assignee successfully added to the task.' })
+  @ApiNotFoundResponse({ description: 'Task or user not found.' })
+  @ApiBadRequestResponse({ description: 'Invalid data or unauthorized action.' })
+  @ApiInternalServerErrorResponse({ description: 'Server error.' })
+  async addAssignee(@Param('id') id: number, @Body() addAssigneeDto: AddAssigneeDto) {
+    return await this.taskService.addAssignee(+id, addAssigneeDto);
   }
 }
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,26 +1,111 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { TaskRepository } from './task.repository';
+import { PaginatedTaskDto } from './dto/paginated-task.dto';
+import { JwtService } from '@nestjs/jwt';
+import { AddTagsDto } from './dto/add-tags.dto';
+import { UserService } from '../user/user.service';
+import { AddAssigneeDto } from './dto/add-assignee.dto';
 
 @Injectable()
 export class TaskService {
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  constructor(
+    private readonly taskRepository: TaskRepository,
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  async findAll(page: number, pageSize: number): Promise<PaginatedTaskDto> {
+    if (page <= 0) {
+      throw new BadRequestException('Invalid page number');
+    }
+
+    const tasks = await this.taskRepository.findAllTasks(page, pageSize);
+
+    return {
+      tasks,
+      currentPage: page,
+      pageSize: tasks.length,
+    };
   }
 
-  findAll() {
-    return `This action returns all task`;
+  async create(createTaskDto: CreateTaskDto, token: string) {
+    if (!token) {
+      throw new BadRequestException(`A token is required for this request`);
+    }
+
+    token = token.split(' ')[1];
+    let userId;
+
+    try {
+      const decodedToken = this.jwtService.decode(token);
+      userId = decodedToken.sub;
+    } catch (error) {
+      throw new BadRequestException(`Invalid token format`);
+    }
+
+    const isMember = await this.taskRepository.checkProjectMembership(createTaskDto.projectId, userId);
+    if (!isMember) {
+      throw new BadRequestException(`Only project members can create tasks`);
+    }
+
+    if (!createTaskDto.tags || createTaskDto.tags.length === 0) {
+      throw new BadRequestException('Tasks must have at least one tag');
+    }
+
+    return await this.taskRepository.createTask(createTaskDto);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} task`;
+
+  async findOne(id: number) {
+    const task = await this.taskRepository.getTaskById(id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    return task;
   }
 
-  update(id: number, updateTaskDto: UpdateTaskDto) {
-    return `This action updates a #${id} task`;
+  async update(id: number, updateTaskDto: UpdateTaskDto) {
+    const task = await this.taskRepository.getTaskById(id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    if (task.status === 'COMPLETED') {
+      throw new BadRequestException('Completed tasks cannot be edited');
+    }
+
+    return await this.taskRepository.updateTask(id, updateTaskDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} task`;
+  async remove(id: number) {
+    const task = await this.taskRepository.getTaskById(id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    return await this.taskRepository.deleteTask(id);
+  }
+
+  async addTagsToTask(taskId: number, addTagsDto: AddTagsDto) {
+    const task = await this.taskRepository.getTaskById(taskId);
+
+    if (!task) {
+      throw new NotFoundException(`Task with id ${taskId} not found`);
+    }
+
+    return await this.taskRepository.addTagsToTask(taskId, addTagsDto.tagIds);
+  }
+
+  async addAssignee(taskId: number, addAssigneeDto: AddAssigneeDto): Promise<void> {
+    const { userId } = addAssigneeDto;
+    const user = await this.userService.findOne(userId);
+
+    await this.taskRepository.addAssignee(taskId, user.id);
   }
 }
